fdc16b13deac2659eb0b4f75a5bc04ebf031bced
Fabio Coradini <fabiosrc@gmail.com>
2023-09-18 14:59:06 -0300

    refactor code, move api call to hook and update ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Notes from "./pages/notes";
 import NavBar from "./components/common/navbar";
 import NotFound from "./pages/notFound";
@@ -16,14 +17,29 @@ function App() {
     logPageView();
   }, []);
 
+  function EditNoteWrapper() {
+    const { id } = useParams();
+    return <EditNote id={id} />;
+  }
+
+  function DetailsNoteWrapper() {
+    const { id } = useParams();
+    return <DetailsNote id={id} />;
+  }
+
+  function DeleteNoteWrapper() {
+    const { id } = useParams();
+    return <DeleteNote id={id} />;
+  }
+
   return (
     <>
       <NavBar />
       <main className="container">
         <Routes>
-          <Route path="/notes/update/:id" element={<EditNote />} />
-          <Route path="/notes/detail/:id" element={<DetailsNote />} />
-          <Route path="/notes/delete/:id" element={<DeleteNote />} />
+          <Route path="/notes/update/:id" element={<EditNoteWrapper />} />
+          <Route path="/notes/detail/:id" element={<DetailsNoteWrapper />} />
+          <Route path="/notes/delete/:id" element={<DeleteNoteWrapper />} />
           <Route path="/notes/new" element={<CreateNote />} />
           <Route path="/notes" element={<Notes />} />
           <Route path="/not-found" element={NotFound} />
--- src/components/noteDisplay.jsx
+++ src/components/noteDisplay.jsx
@@ -1,14 +1,16 @@
 import { Card, CardContent, Typography } from "@mui/material";
 
-const NoteDisplay = ({ title, body }) => (
-  <Card>
-    <CardContent>
-      <Typography variant="h5" component="div">
-        {title}
-      </Typography>
-      <Typography variant="body2">{body}</Typography>
-    </CardContent>
-  </Card>
-);
+const NoteDisplay = ({ title, body }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2">{body}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default NoteDisplay;
